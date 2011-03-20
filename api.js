49731cb855ca4464a8bd1a3aaa3cf0d5da9f72e8
Andrew <AndrewSumin@gmail.com>
2011-03-20 18:51:52 +0300

    add documentation

--- api.js
+++ api.js
@@ -3,35 +3,11 @@
     var host = 'http://api.hh.ru/1/json';
     var counter = 0;
 
-    var A = Array.prototype;
-    if (!A.indexOf) {
-        A.indexOf = function (searchElement, fromIndex) {
-            fromIndex = fromIndex || 0;
-            for (var length = this.length; fromIndex < length; fromIndex++) {
-                if (this[fromIndex] == searchElement) {
-                    return fromIndex;
-                }
-            }
-            return -1;
-        };
-    }
-    if (!A.lastIndexOf) {
-        A.lastIndexOf = function (searchElement, fromIndex) {
-            var length = this.length;
-            fromIndex = fromIndex || length - 1;
-            if (fromIndex < 0) {
-                fromIndex += length;
-            }
-            for (; fromIndex >= 0; fromIndex--) {
-                if (this[fromIndex] == searchElement) {
-                    return fromIndex;
-                }
-            }
-            return -1;
-        };
-    }
-    if (!A.every) {
-        A.every = function (callback, thisObject) {
+    if (!Array.prototype.every) {
+        /**
+         * @ignore
+         */
+        Array.prototype.every = function (callback, thisObject) {
             thisObject = thisObject || window;
             var index = 0, length = this.length;
             for (; index < length; index++) {
@@ -42,8 +18,12 @@
             return (index == length);
         };
     }
-    if (!A.filter) {
-        A.filter = function (callback, thisObject) {
+
+    if (!Array.prototype.filter) {
+        /**
+         * @ignore
+         */
+        Array.prototype.filter = function (callback, thisObject) {
             thisObject = thisObject || window;
             var length = this.length, count = 0, filtered = [];
             for (var index = 0; index < length; index++) {
@@ -55,16 +35,24 @@
             return filtered;
         };
     }
-    if (!A.forEach) {
-        A.forEach = function (callback, thisObject) {
+
+    if (!Array.prototype.forEach) {
+        /**
+         * @ignore
+         */
+        Array.prototype.forEach = function (callback, thisObject) {
             thisObject = thisObject || window;
             for (var index = 0, length = this.length; index < length; index++) {
                 callback.apply(thisObject, [this[index], index, this]);
             }
         };
     }
-    if (!A.map) {
-        A.map = function (callback, thisObject) {
+
+    if (!Array.prototype.map) {
+        /**
+         * @ignore
+         */
+        Array.prototype.map = function (callback, thisObject) {
             thisObject = thisObject || window;
             var length = this.length, map = [];
             for (var index = 0; index < length; index++) {
@@ -73,8 +61,12 @@
             return map;
         };
     }
-    if (!A.some) {
-        A.some = function (callback, thisObject) {
+
+    if (!Array.prototype.some) {
+        /**
+         * @ignore
+         */
+        Array.prototype.some = function (callback, thisObject) {
             thisObject = thisObject || window;
             var index = 0, length = this.length;
             for (; index < length; index++) {
@@ -85,15 +77,6 @@
             return (index != length);
         };
     }
-    if (!A.remove) {
-        // Array Remove - By John Resig (MIT Licensed)
-        A.remove = function(from, to) {
-            var rest = this.slice((to || from) + 1 || this.length);
-            this.length = from < 0 ? this.length + from : from;
-            return this.push.apply(this, rest);
-        };
-    }
-
 
     var utils = {
         getCounter: function(){
@@ -136,34 +119,130 @@
         }
     };
 
-    var helper = {};
+    /**
+     * @name api
+     * @namespace Holds functionality.
+     */
+    var api = {};
 
-    helper.vacancy = function(vacancy){
+    /**
+     * @constructor
+     * @example
+     * // JSON example
+     * {
+     *     "id":"4069864",
+     *     "link":[
+     *         {
+     *             "rel":"self",
+     *             "type":"application/json",
+     *             "href":"http://api.hh.ru/1/json/vacancy/4069864/"
+     *         },{
+     *             "rel":"alternate",
+     *             "type":"text/html",
+     *             "href":"http://hh.ru/vacancy/4069864"
+     *         }
+     *     ],
+     *     "name":"PHP программист",
+     *     "employer":{
+     *         "id":"1455",
+     *         "link":[
+     *             {
+     *                 "rel":"self",
+     *                 "type":"application/json",
+     *                 "href":"http://api.hh.ru/1/json/employer/1455/"
+     *             },{
+     *                 "rel":"alternate",
+     *                 "type":"text/html",
+     *                 "href":"http://hh.ru/employer/1455"
+     *             }
+     *         ],
+     *         "name":"HeadHunter",
+     *         "department":{
+     *             "code":"hh-1455-TECH",
+     *             "link":[
+     *                 {
+     *                     "type":"text/html",
+     *                     "href":"http://hh.ru/employer/1455?dpt=hh-1455-TECH"
+     *                 }
+     *             ],
+     *             "name":"HeadHunter::Технический департамент"
+     *         }
+     *     },
+     *     "update":{
+     *         "timestamp":"1300178027"
+     *     },
+     *     "salary":{
+     *         "from":"80000",
+     *         "currency":{
+     *             "code":"RUR",
+     *             "name":"руб."
+     *         }
+     *     },
+     *     "region":{
+     *         "id":"1",
+     *         "name":"Москва"
+     *     },
+     *     "site":"http://hh.ru"
+     * }
+     */
+
+    api.vacancy = function(vacancy){
         var links = {}
         for (var i =0; i < vacancy.link.length; i++){
             links[vacancy.link[i].rel] = vacancy.link[i];
+            if (vacancy.link[i].salary.currency && vacancy.link[i].salary.currency.__text){
+                vacancy.link[i].salary.currency.name = vacancy.link[i].salary.currency__text;
+            }
         }
         vacancy.link = links;
         return vacancy;
     };
 
-    helper.search = {
+    /**
+     * @namespace Holds search result object.
+     * @ignore
+     */
+    api.search = {
+        /**
+         * @private
+         */
         init: function(json, query){
             this.found = json.found;
             this.query = query;
             this.query.page = query.page || 0;
-            this.pager = new helper.pager(this);
+            this.pager = new api.pager(this);
         }
     };
-    helper.search.vacancy = function(json, query){
-        this.vacancies = json.vacancies.map(helper.vacancy);
+
+    /**
+     * @constructor
+     * @description Search vacancy result object.
+     * @property {Array} vacancies List of {@link api.vacancy}
+     * @property {Object} pager Pager, see {@link api.pager}
+     * @property {Number} found number of {@link api.vacancy}
+     * @param json JSON response from api
+     * @param query Hash of query params
+     */
+    api.search.vacancy = function(json, query){
+        this.vacancies = json.vacancies.map(api.vacancy);
         this.init(json, query);
     };
-    helper.search.vacancy.prototype = helper.search;
+    api.search.vacancy.prototype = api.search;
 
-    helper.pager = function(vacancySearchResult){
+    /**
+     * @constructor
+     * @param object result of search {@link api.search.vacancy}
+     * @property {Number} pages total number of pages
+     */
+
+    api.pager = function(vacancySearchResult){
         this.page = vacancySearchResult.query.page;
         this.pages = Math.ceil(vacancySearchResult.found / vacancySearchResult.query.items || 20);
+
+        /**
+         * @description Get previous page of vacancies
+         * @param callback Receives a {@link api.search.vacancy}
+         */
         this.prev = function(callback){
             if (this.page <= 0){
                 return null;
@@ -171,6 +250,10 @@
             vacancySearchResult.query.page--;
             api.vacancies.search(vacancySearchResult.query, callback);
         };
+        /**
+         * @description Get next page of vacancies
+         * @param callback Receives a {@link api.search.vacancy}
+         */
         this.next = function(callback){
             if (this.page >= this.pages){
                 return null;
@@ -178,6 +261,10 @@
             vacancySearchResult.query.page++;
             api.vacancies.search(vacancySearchResult.query, callback);
         };
+        /**
+         * @description Get exact page of vacancies
+         * @param callback Receives a {@link api.search.vacancy}
+         */
         this.exact = function(page){
             if (page <= 0 || page >= this.pages){
                 return null;
@@ -187,20 +274,49 @@
         };
     };
 
-    var api = {};
-
+    /**
+     * @namespace Holds vacancies functionality.
+     */
     api.vacancies = {};
 
+    /**
+     * @description Search vacancies by query, callback receive {@link api.search.vacancy}.
+     * @param query Hash with query params
+     * @param callback CallBack function, receive {@link api.search.vacancy}
+     * @example
+     * api.vacancies.employer({text:'javascript', region:[1, 2]},
+     *     function(result){
+     *         function alertName (vacancy){
+     *             alert (vacancy.name)
+     *         }
+     *         result.vacancies.forEach(alertName)
+     *     }
+     * )
+     */
     api.vacancies.search = function(query, callback) {
         var callbackName = utils.createCallback(function(json){
-            callback(new helper.search.vacancy(json, query));
+            callback(new api.search.vacancy(json, query));
         });
         utils.createScript({src: utils.createSrc('/vacancy/search/', query, callbackName)});
     };
 
+    /**
+     * @description Get employer vacancies, callback receive array of {@link api.vacancy}.
+     * @param id Employer id
+     * @param callback CallBack function, receive array of {@link api.vacancy}
+     * @example
+     * api.vacancies.employer(1455,
+     *     function(vacancies){
+     *         function alertName (vacancy){
+     *             alert (vacancy.name)
+     *         }
+     *         vacancies.forEach(alertName)
+     *     }
+     * )
+     */
     api.vacancies.employer = function(id, callback) {
         var callbackName = utils.createCallback(function(json){
-            callback(json.map(helper.vacancy));
+            callback(json.map(api.vacancy));
         });
         utils.createScript({src: utils.createSrc('/vacancy/employer/' + id + '/', {}, callbackName)});
     };
